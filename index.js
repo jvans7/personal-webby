f1381fc880da1fce68d5b1552a04a4cefc0f6f92
jvans7 <jevaughn_footballer@hotmail.com>
2016-05-31 03:58:21 -0400

    https

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+var host = "jvans7.github.io";
+if ((host == window.location.host) && (window.location.protocol != "https:"))
+    window.location.protocol = "https";
+
+
 $(document).ready(function(){
   $(".well").hover(function(){
     $(".well").css("opacity", .9);
