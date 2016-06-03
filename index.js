12c50c5a9d3ad1122a0bf9e8071bb87a4c03ec88
jvans7 <jevaughn_footballer@hotmail.com>
2016-06-02 22:40:00 -0400

    Wikipedia search

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var host = "jvans7.github.io";
-if ((host == window.location.host) && (window.location.protocol != "https:"))
-    window.location.protocol = "https";
-
-
 $(document).ready(function(){
   $(".well").hover(function(){
     $(".well").css("opacity", .9);
